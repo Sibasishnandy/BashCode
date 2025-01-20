e681299a42952765a127ac0e99d5f80a1ad04c52
Sibasishnandy <142724809+Sibasishnandy@users.noreply.github.com>
2025-01-20 11:40:24 +0530

    Add files via upload

---
+++ 5a.sh
@@ -0,0 +1,14 @@
+x="words.txt"
+while IFS=' ' read -r line ; do
+  for word in $line ; do
+  clean=$(echo "$word" | tr -d ',.;')
+  reverse=$(echo "$clean" | rev)
+
+  if [ $reverse = $clean ] ; then
+  echo "Palindrome"
+  else
+  echo "Not"
+  fi
+
+  done
+done < "$x"
